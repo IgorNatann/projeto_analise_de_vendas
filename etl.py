5549a2f6a08a24d54cab0e127be6e4d86cc25143
IgorNatann <Igornatan4@gmail.com>
2026-02-24 18:03:09 -0300

    feat(etl): adiciona função para somar valores dos produtos não entregues

--- etl.py
+++ etl.py
@@ -32,9 +32,24 @@ def filtrar_produtos_nao_entregues(lista: list[dict]) -> list[dict]:
     return lista_produtos_filtrados
 
 
+def somar_valores_dos_produtos_nao_entregues(filtrar_produtos_nao_entregues: list[dict]) -> int:
+    
+    """
+    Função para somar total dos valores dos produtos não entregues.
+    """
+
+    valor_total = 0
+
+    for produto in filtrar_produtos_nao_entregues:
+        valor_total += int(produto.get('PREÇO'))          
+    return valor_total
+
+
 lista_de_produtos = ler_csv(path_arquivo)
 lista_produtos_filtrados = filtrar_produtos_nao_entregues(lista_de_produtos)
+valor_total = somar_valores_dos_produtos_nao_entregues(lista_produtos_filtrados)
 
 
-print(lista_de_produtos)
-print(lista_produtos_filtrados)
+print(f"Total de produtos vendidos: {len(lista_de_produtos)}")
+print(f"Total de produtos não entregues: {len(lista_produtos_filtrados)}")
+print(f"Valor total dos produtos não entregues: R$ {valor_total}")
