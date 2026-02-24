656751922f5accc0c89c1da6cdadce3f3bb63765
IgorNatann <Igornatan4@gmail.com>
2026-02-24 18:11:15 -0300

    feat(main): adiciona script principal para processamento de vendas

--- etl.py
+++ etl.py
@@ -43,13 +43,3 @@ def somar_valores_dos_produtos_nao_entregues(filtrar_produtos_nao_entregues: lis
     for produto in filtrar_produtos_nao_entregues:
         valor_total += int(produto.get('PREÇO'))          
     return valor_total
-
-
-lista_de_produtos = ler_csv(path_arquivo)
-lista_produtos_filtrados = filtrar_produtos_nao_entregues(lista_de_produtos)
-valor_total = somar_valores_dos_produtos_nao_entregues(lista_produtos_filtrados)
-
-
-print(f"Total de produtos vendidos: {len(lista_de_produtos)}")
-print(f"Total de produtos não entregues: {len(lista_produtos_filtrados)}")
-print(f"Valor total dos produtos não entregues: R$ {valor_total}")
